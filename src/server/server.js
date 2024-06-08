08159f78193b646a7f006d599833ccc1db9d4676
ChristianP5 <119984279+ChristianP5@users.noreply.github.com>
2024-06-08 14:09:48 +0700

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,33 @@
 const Hapi = require('@hapi/hapi');
+const dotenv = require('dotenv');
+const routes = require('./routes');
+
+dotenv.config();
 
 const init = async ()=>{
     const server = Hapi.server({
         port: 9000,
         host: 'localhost',
     })
+
+    server.ext('onPreResponse',(request, h)=>{
+        const response = request.response;
+
+        if(response instanceof Error){
+            const newResponse = h.response({
+                status: 'fail',
+                message: 'Returned an Error!',
+                error: response.stack,
+            })
+
+            newResponse.code(500);
+            return newResponse;
+        }
+
+        return h.continue;
+    })
+
+    server.route(routes);
 
     await server.start();
     console.log(`Server started at ${server.info.uri}`);
